5edff1bdfecf15df836878d01c45d035a2533938
luis13211 <165101448+luis13211@users.noreply.github.com>
2025-02-18 17:51:17 -0500

    Refactor findAvaiblePort function for improved readability and consistent indentation

--- freePort.js
+++ freePort.js
@@ -1,22 +1,22 @@
 import { createServer } from 'node:net';
 
 export function findAvaiblePort(desiredPort) {
-    return new Promise((resolve, reject) => {
-        const server = createServer();
+  return new Promise((resolve, reject) => {
+    const server = createServer();
 
-        server.listen(desiredPort, () => {
-            const port = server.address();
-            server.close(() => {
-                resolve(port);
-            });
-        })
-
-        server.on('error', (err) => {
-            if (err.code === 'EADDRINUSE') {
-                resolve(findAvaiblePort(0)).then(resolve);
-            } else {
-                reject(err);
-            }
-        })
+    server.listen(desiredPort, () => {
+      const port = server.address();
+      server.close(() => {
+        resolve(port);
+      });
     })
+
+    server.on('error', (err) => {
+      if (err.code === 'EADDRINUSE') {
+        resolve(findAvaiblePort(0)).then(resolve);
+      } else {
+        reject(err);
+      }
+    })
+  })
 }
