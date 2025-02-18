c2caafa6be9b09ac0240b59f0f4f84303cf6a72e
luis13211 <165101448+luis13211@users.noreply.github.com>
2025-02-18 17:21:26 -0500

    Refactor index.js to use ES6 imports, set up JSON middleware, and handle 404 errors

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-const express = require('express')
-const app = express()
+import express, { json } from 'express';
+const app = express();
+const port = 8080;
 
-/*app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.disable('x-powered-by');
+app.use(json());
 
-app.listen(3000)*/
 
-app.listen(8080, () => {
-    console.log("Servidor en linea")
-  })
+app.use((req, res) =>{
+  res.status(404).send('404 not found')
+  console.log(req.url)
+  console.log('404 not found')
+});
+
+app.listen(port, () => {
+  console.log(`Servidor en Linea en http://localhost:${port}`);
+});
