4222cd5dde8e32fe42876f59ecb588421168d9db
L <160187610+kurizuki@users.noreply.github.com>
2025-02-19 00:14:13 -0500

    Add basic route handlers for login, register, logout, and protected endpoints

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ const port = 8080;
 app.disable('x-powered-by');
 app.use(json());
 
+app.get('/',(req, res) => {
+  res.send('Hola mundo!!!!')
+})
+
+app.post('/login',(req, res) => {
+
+})
+app.post('/register',(req, res) => {
+
+})
+app.post('/logout',(req, res) => {
+
+})
+
+app.get('/protected',(req, res) => {
+
+})
 
 app.use((req, res) =>{
   res.status(404).send('404 not found')
