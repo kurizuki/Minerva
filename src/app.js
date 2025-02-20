f173fface79a34516a1c3b02cd42ff6285a6167d
L <160187610+kurizuki@users.noreply.github.com>
2025-02-19 20:27:40 -0500

    Update entry point to src/app.js

--- src/app.js
+++ src/app.js
@@ -7,26 +7,30 @@ app.use(json());
 
 app.get('/',(req, res) => {
   res.send('Hola mundo!!!!')
-})
+});
 
 app.post('/login',(req, res) => {
 
-})
+});
 app.post('/register',(req, res) => {
+  const {email, password} = req.body;
 
-})
+  console.log(email + password);
+  res.send("ho");
+
+});
 app.post('/logout',(req, res) => {
 
-})
+});
 
 app.get('/protected',(req, res) => {
 
-})
+});
 
 app.use((req, res) =>{
-  res.status(404).send('404 not found')
-  console.log(req.url)
-  console.log('404 not found')
+  res.status(404).send('404 not found');
+  console.log(req.url);
+  console.log('404 not found');
 });
 
 app.listen(port, () => {
