b97c876e77cc18944122d927583e055cb329681a
L <160187610+kurizuki@users.noreply.github.com>
2025-02-21 13:25:51 -0500

    Refactor app routes by integrating customer routers and cleaning up unused endpoints

--- src/app.js
+++ src/app.js
@@ -1,34 +1,14 @@
 import express, { json } from 'express';
+import { customerRouters } from './routes/customer.js';
 
 export const app = express();
 
 app.disable('x-powered-by');
 app.use(json());
 
-app.get('/',(req, res) => {
-  res.send('Hola mundo!!!!')
-});
-
-app.post('/login',(req, res) => {
-
-});
-app.post('/register',(req, res) => {
-  const {email, password} = req.body;
-
-  console.log(email + password);
-  res.send("ho");
-
-});
-app.post('/logout',(req, res) => {
-
-});
-
-app.get('/protected',(req, res) => {
-
-});
+app.use('/customer', customerRouters);
 
 app.use((req, res) =>{
   res.status(404).send('404 not found');
-  console.log(req.url);
-  console.log('404 not found');
+  console.log("404 not found:" + req.url);
 });
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import app from './app';
+import { app } from './app.js';
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
-    console.log(`Servidor en Linea en el puerto ${port}`);
-  });
+  console.log(`Servidor en Linea en el puerto ${port}`);
+});
   
