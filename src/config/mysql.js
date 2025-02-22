e705956c01397ffc7403633fae0500dbb6f5ed4e
L <160187610+kurizuki@users.noreply.github.com>
2025-02-22 14:02:12 -0500

    Add MySQL connection pool configuration

---
+++ src/config/mysql.js
@@ -0,0 +1,16 @@
+import mysql from 'mysql2/promise';
+
+export const connection = mysql.createPool({
+  host:'localhost',
+  port:'3306',
+  database: 'thot',
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+
+  //Si todas las conexiones estan en uso, las solicitudes adicionales se quedaran en cola
+  waitForConnections: true,
+  //El numero maximo de conexiones simultaneas que el pool puede crear
+  connectionLimit: 10,
+  //Con un valor de 0, se establece que no hay límite en la cantidad de solicitudes que se pueden poner en cola. 
+  queueLimit: 0
+});
