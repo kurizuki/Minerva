21f4d909ae0d70fdf9102618d78feaf099f3224e
L <160187610+kurizuki@users.noreply.github.com>
2025-02-22 15:19:37 -0500

    Add validation functions for customer login and registration using Zod schema

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,9 +1,19 @@
-export const register = (req, res) => {
+import { validateCustomerLogin, validateCustomerRegister } from "../schemas/customer";
 
+export const register = (req, res) => {
+  const result  = validateCustomerRegister(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({error: JSON.parse(result.error.message)});
+  }
 };
 
 export const login = (req, res) => {
+  const result  = validateCustomerLogin(req.body);
 
+  if (!result.success) {
+    return res.status(400).json({error: JSON.parse(result.error.message)});
+  }
 };
 
 export const logout = (req, res) => {
--- src/schemas/customer.js
+++ src/schemas/customer.js
@@ -1,6 +1,28 @@
 import {z} from 'zod';
 
-const customerSchema = z.object({
+export function validateCustomerLogin(object) {
+  return customerLoginSchema.safeParse(object);
+}
+
+export function validateCustomerRegister(object) {
+  return customerRegisterSchema.safeParse(object);
+}
+
+const customerLoginSchema = z.object({
+  email: z
+    .string({ required_error: 'El email es requerido' })
+    .trim()
+		.min(1, 'El email no puede estar vacío')
+		.max(50, 'El email no puede tener mas de 50 caracteres')
+    .email('El email debe ser válido'),
+  password: z
+    .string({ required_error: 'La contraseña es requerida' })
+    .min(4, 'La contraseña debe tener al menos 4 caracteres')
+		.max(22, 'La contraseña como maximo puede tener 22 caracteres')
+		.regex(/^[a-zA-Z0-9\s]+$/, 'La contraseña solo puede contener letras, espacios y números')
+});
+
+const customerRegisterSchema = z.object({
   fullName: z
     .string({ required_error: 'El nombre completo es requerido' })
     .refine(value => value.trim().length > 0, {
@@ -27,8 +49,5 @@ const customerSchema = z.object({
     .min(4, 'La contraseña debe tener al menos 4 caracteres')
 		.max(22, 'La contraseña como maximo puede tener 22 caracteres')
 		.regex(/^[a-zA-Z0-9\s]+$/, 'La contraseña solo puede contener letras, espacios y números')
-	});
+});
 
-export function validateCustomer(object) {
-  return customerSchema.safeParse(object);
-}
