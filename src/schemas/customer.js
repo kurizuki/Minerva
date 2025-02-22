793e663cb63ab44ca6ab721bdba4e5ce4cff8184
L <160187610+kurizuki@users.noreply.github.com>
2025-02-22 14:21:32 -0500

    Enhance customer schema validation with regex for full name and password

--- src/schemas/customer.js
+++ src/schemas/customer.js
@@ -4,13 +4,16 @@ export const customerSchema = z.object({
   fullName: z
     .string({ required_error: 'El nombre completo es requerido' })
     .min(1, 'El nombre completo no puede estar vacío')
-		.max(50, 'El nombre completo no puede tener mas de 50 caracteres'),
+		.max(50, 'El nombre completo no puede tener mas de 50 caracteres')
+		.regex(/^[a-zA-Z\s]+$/, 'El nombre solo puede contener letras y espacios'),
   email: z
     .string({ required_error: 'El email es requerido' })
-    .email('El email debe ser válido')
-		.max(50, 'El email no puede tener mas de 50 caracteres'),
+		.min(1, 'El email no puede estar vacío')
+		.max(50, 'El email no puede tener mas de 50 caracteres')
+    .email('El email debe ser válido'),
   password: z
     .string({ required_error: 'La contraseña es requerida' })
     .min(4, 'La contraseña debe tener al menos 4 caracteres')
 		.max(22, 'La contraseña como maximo puede tener 22 caracteres')
-});
+		.regex(/^[a-zA-Z0-9\s]+$/, 'La contraseña solo puede contener letras, espacios y números')
+	});
