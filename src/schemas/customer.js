52587a875a0fbdb0f46df19d81d88be3985a149d
L <160187610+kurizuki@users.noreply.github.com>
2025-02-22 19:04:21 -0500

    Rixed a error of customer schema validation and simplify error messages

--- src/schemas/customer.js
+++ src/schemas/customer.js
@@ -12,40 +12,31 @@ const customerLoginSchema = z.object({
   email: z
     .string({ required_error: 'El email es requerido' })
     .trim()
-		.min(1, 'El email no puede estar vacío')
-		.max(50, 'El email no puede tener mas de 50 caracteres')
-    .email('El email debe ser válido'),
+		.min(1)
+		.max(50)
+    .email(),
   password: z
     .string({ required_error: 'La contraseña es requerida' })
-    .min(4, 'La contraseña debe tener al menos 4 caracteres')
-		.max(22, 'La contraseña como maximo puede tener 22 caracteres')
-		.regex(/^[a-zA-Z0-9\s]+$/, 'La contraseña solo puede contener letras, espacios y números')
+    .min(1)
+		.max(22)
+		.regex(/^[a-zA-Z0-9\s]+$/)
 });
 
 const customerRegisterSchema = z.object({
   fullName: z
     .string({ required_error: 'El nombre completo es requerido' })
-    .refine(value => value.trim().length > 0, {
-      message: 'El nombre completo no puede ser solo espacios en blanco'
-    })
     .trim()
     .min(1, 'El nombre completo no puede estar vacío')
 		.max(50, 'El nombre completo no puede tener mas de 50 caracteres')
 		.regex(/^[a-zA-Z\s]+$/, 'El nombre solo puede contener letras y espacios'),
   email: z
     .string({ required_error: 'El email es requerido' })
-    .refine(value => value.trim().length > 0, {
-      message: 'El email no puede ser solo espacios en blanco'
-    })
     .trim()
 		.min(1, 'El email no puede estar vacío')
 		.max(50, 'El email no puede tener mas de 50 caracteres')
     .email('El email debe ser válido'),
   password: z
     .string({ required_error: 'La contraseña es requerida' })
-    .refine(value => value.trim().length > 0, {
-      message: 'La contraseña no puede ser solo espacios en blanco'
-    })
     .min(4, 'La contraseña debe tener al menos 4 caracteres')
 		.max(22, 'La contraseña como maximo puede tener 22 caracteres')
 		.regex(/^[a-zA-Z0-9\s]+$/, 'La contraseña solo puede contener letras, espacios y números')
