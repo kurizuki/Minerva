3f395c0d35b1349b0f9fea9802f19fb71390733b
L <160187610+kurizuki@users.noreply.github.com>
2025-02-26 19:50:26 -0500

    Update CORS middleware to allow additional origin and set proper headers

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -1,13 +1,15 @@
 const acceptedOrigins = [
-  'http://localhost:8080'
+  'http://localhost:8080',
+  'http://127.0.0.1:3000'
 ];
 
 export function corsMiddleware(req, res, next) {
   const origin = req.header('origin');
 
   if(acceptedOrigins.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Origin', 'GET, POST, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   }
   next();
 }
